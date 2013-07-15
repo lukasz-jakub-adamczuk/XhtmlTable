8f975812725ec4ee020dcb8f48f56a0a9e3f6097
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2013-07-16 00:02:05 +0200

    adding Bool Cell

---
+++ AyaXhtmlTableCellBool.php
@@ -0,0 +1,14 @@
+<?php
+
+class AyaXhtmlTableCellBool extends AyaXhtmlTableCell {
+
+    protected $_aTags;
+    
+    protected function _beforeConfigure() {
+        $this->_sAlign = 'center';
+    }
+    
+    /* column operations */
+
+
+}
