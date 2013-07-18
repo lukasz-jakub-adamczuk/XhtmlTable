ee8e9796dfc64e6121fd66c9fa868a170b9ab1dc
Lukasz Adamczuk <lukasz.jakub.adamczuk@gmail.com>
2013-07-18 11:48:07 +0200

    yaml loader and lang supprt

---
+++ AyaYamlLoader.php
@@ -0,0 +1,39 @@
+<?php
+
+class AyaSingleton {
+	
+	protected static $_instance;
+
+	protected function __construct() {}
+
+	protected function __clone() {}
+
+	public static function getInstance() {
+	    if(is_null(self::$_instance)) {
+	        self::$_instance = new self();
+	    }
+	    return self::$_instance;
+	}
+
+
+}
+
+
+class AyaYamlLoader extends AyaSingleton {
+
+	public static function getInstance() {
+	    if (is_null(self::$_instance)) {
+	    	require_once __DIR__ . '/vendor/yaml/sfYamlParser.php';
+	        self::$_instance = new sfYamlParser();
+	    }
+	    return self::$_instance;
+	}
+
+	public static function parse($file) {
+		
+		$oYamlParser = self::getInstance();
+		
+		return $oYamlParser->parse(file_get_contents($file));
+	}
+
+}
